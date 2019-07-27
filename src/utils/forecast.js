ace22b279a3c2c7dcef804bab3c7a3bbea554e39
StK1729 <stefan.kraljevic@gmail.com>
2019-07-27 08:57:33 +0200

    added additional weather information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require("request");
 
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `https://api.darksky.net/forecast/4d274e8131494d5c11923ac5f9ef0abc/${latitude},${longitude}?units=si&si=temperature`;
+    const url = `https://api.darksky.net/forecast/4d274e8131494d5c11923ac5f9ef0abc/${latitude},${longitude}?units=si`;
 
     request({
         url,
@@ -13,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback(body.error);
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It's currently ${body.currently.temperature} degrees out there. There is a ${body.currently.precipProbability}% probability of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It's currently ${body.currently.temperature} \u2103 out there. There is a ${body.currently.precipProbability}% probability of rain. The lowest temperature today will be ${body.daily.data[0].temperatureMin} \u2103, and the hightest ${body.daily.data[0].temperatureMax} \u2103.`);
         }
     })
 }
